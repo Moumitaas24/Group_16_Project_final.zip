65006324869c5810dc466a4b4c369f9ff0736a8e
MD Saif Ahmed Shishir <shsihirsaif216837@gmail.com>
2025-04-17 23:44:23 +0600

    Dashboard

---
+++ src/main/java/MD_Saif_Ahmed_Shishir/AccountOfficerDashboard.java
@@ -0,0 +1,34 @@
+package MD_Saif_Ahmed_Shishir;
+
+import javafx.event.ActionEvent;
+
+public class AccountOfficerDashboard
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void BonusButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TaxReportButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LeaveApplicationButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SalaryButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void IncomeStatementButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BudgetButton(ActionEvent actionEvent) {
+    }
+}
